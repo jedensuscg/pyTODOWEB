4ad48b7b90327519cdd05cea5a0030e45e0a9ee1
James Edens <jedensuscg@gmail.com>
2023-07-04 13:13:05 -0500

    added download link

--- web_todo.py
+++ web_todo.py
@@ -98,7 +98,9 @@ with st.sidebar:
     st.button("Delete Marked", key="delete_marked_button", on_click=delete_marked)
     display_filelist()
     st.write("Currently, adding or deleting lists in the web version is not possible.")
-    st.write("Use the desktop version for full functionality.")
+    st.write("Use the desktop version for full functionality. The desktop version can be downloaded from my github")
+    st.write('[Download Here](https://github.com/jedensuscg/pyTODOWEB/releases/tag/v1.0.0)')
+    st.write('The desktop version includes a full command line and UI options.')
 
 
 with main:
